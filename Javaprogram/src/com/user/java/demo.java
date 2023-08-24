7a4f4e01f0d9ef3ab3bbf78520ae1b3e0dd20469
DIVYA <DIVYA@LAPTOP-E1RQTFE0>
2023-08-24 21:02:53 +0530

    first commit

---
+++ Javaprogram/src/com/user/java/demo.java
@@ -0,0 +1,10 @@
+package com.user.java;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Javaprogram/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author DIVYA
+ *
+ */
+module Javaprogram {
+}
