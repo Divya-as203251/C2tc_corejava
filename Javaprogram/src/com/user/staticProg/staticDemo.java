da8f6c596ec66947fc4d7413614132b913bb4e3e
DIVYA <DIVYA@LAPTOP-E1RQTFE0>
2023-09-04 10:08:02 +0530

    Third commit

---
+++ Javaprogram/src/com/user/staticProg/staticDemo.java
@@ -0,0 +1,14 @@
+package com.user.staticProg;
+
+public class staticDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(Employee.companyName);
+Employee e=new Employee("sama",101);
+System.out.println(e);
+Employee e1=new Employee("ramya",103);
+System.out.println(e1);
+	}
+
+}
