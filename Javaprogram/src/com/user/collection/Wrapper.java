d827d5a0d427587bb86b18ac713b8b273a2b24b7
DIVYA <DIVYA@LAPTOP-E1RQTFE0>
2023-09-25 10:09:06 +0530

    collections

---
+++ Javaprogram/src/com/user/collection/Wrapper.java
@@ -0,0 +1,13 @@
+package com.user.collection;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer intValue=100;
+		String stringobj=intValue.toString();
+		System.out.println(stringobj.length());
+
+	}
+
+}
