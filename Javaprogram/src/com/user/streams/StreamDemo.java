a3618596cafcabddc0be26086ee50d22b5955587
DIVYA <DIVYA@LAPTOP-E1RQTFE0>
2023-10-08 12:29:41 +0530

    streams

---
+++ Javaprogram/src/com/user/streams/StreamDemo.java
@@ -0,0 +1,12 @@
+package com.user.streams;
+
+public class StreamDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StreamOperations ob=new StreamOperations();
+		ob.allOperations();
+
+	}
+
+}
---
+++ Javaprogram/src/com/user/streams/StreamFunctionsDemo.java
@@ -0,0 +1,12 @@
+package com.user.streams;
+
+public class StreamFunctionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StreamFunctions ob=new StreamFunctions();
+		ob.streamFunctions();
+
+	}
+
+}
